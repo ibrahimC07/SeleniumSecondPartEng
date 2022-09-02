16bce3655cde097129d27e395b59c3c9d0d66fdc
Atiye Kariparduc <atiyeozerbr@gmail.com>
2022-09-02 16:07:17 -0400

    WebTables Interview

---
+++ src/test/java/Practice/WebTablesInterview.java
@@ -0,0 +1,35 @@
+package Practice;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class WebTablesInterview extends TestBase {
+@Test
+    public void test(){
+        driver.get("https://the-internet.herokuapp.com/tables");
+        List<WebElement>allTable=driver.findElements(By.xpath("//table[@id='table1']"));
+        //How do you get the element in the all column?
+        List<WebElement>allColumns=driver.findElements(By.xpath("//table[@id='table1']//tr//th"));
+        //How do you get the element in the all rows?
+        List<WebElement>allRows=driver.findElements(By.xpath("//table[@id='table1']//tr"));
+        //How do you get the last row table data?
+       int allRowsSize= allRows.size();
+        System.out.println("lastRow: "+allRows.get(allRowsSize-1).getText());
+        //How do you get the element in the last column?
+          int columnSize=allColumns.size();
+    System.out.println( "last column:"+allColumns.get(columnSize-1).getText());
+        //How do you get the last table data?
+    WebElement LastCell=driver.findElement(By.xpath("//table[@id='table1']//tr["+(allRowsSize-1)+"]//td["+(columnSize)+"]"));
+    System.out.println("LastCell:"+LastCell.getText());
+
+       }
+
+
+
+
+    }
+
